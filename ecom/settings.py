72b5917226cd4a787eb656debe6281566fb2a4a1
Shivam Kushwaha <shivamk8368@gmail.com>
2021-01-04 13:04:12 -0500

    adding static files

--- ecom/settings.py
+++ ecom/settings.py
@@ -172,9 +172,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 
 
